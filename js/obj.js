5b0ed540509e0de5fce641922b0f5c233b0389a4
orozcoding <orozcoding@gmail.com>
2022-01-30 04:53:30 -0400

    improve mobile version

--- js/obj.js
+++ js/obj.js
@@ -14,7 +14,7 @@ const obj = {
   bonus2: false,
   bonus4: false,
   bonus6: false,
-  ver: 'a-0.1.4'
+  ver: 'a-0.1.1'
 }
 
 const api = 'https://api.pancakeswap.info/api/v2/tokens/0xfa57ff4670f9016069e799a51a3186d03991e431'
